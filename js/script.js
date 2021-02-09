ef4b28bfbb1473a9dd54603be2e5dcaec035b434
ichionse_hiroki <ichionse_hiroki@Yuta-Ichinose.local>
2021-02-10 01:44:19 +0900

    画像右の余白設定が不完全

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 $(function () {
-    
+    $('.p-language-select').on('click', function () {
+        $('.p-language-text-wrap').show();
+        return false;
+        // $('.p-language-text-wrap').css('display', 'block');
+        
+        // $('body').on('click', function () {
+        //     $('.p-language-text-wrap').hide();
+        // });
+    });
 
 
-
-
-})
+});
