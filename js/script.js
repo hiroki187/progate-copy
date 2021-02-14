d660d95323fa2d9f7fa3ba9b4774e8e22ee886de
ichionse_hiroki <ichionse_hiroki@Yuta-Ichinose.local>
2021-02-14 20:32:29 +0900

    最終確認

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 $(function () {
     $('.p-top-language-text-wrap').hide();
     $('.p-top-language-select').on('click', function () {
-        $('.p-top-language-text-wrap').toggle();
+            $('.p-top-language-text-wrap').toggle();
         return false;
-        
-        // $('.p-language-text-wrap').css('display', 'block');
-        
     });
-
-
+    
+    $(document).on('click', function(event) {
+        if(!$(event.target).closest('.p-top-language-text-wrap').length) {
+            console.log('外側がクリックされました。');
+            $('.p-top-language-text-wrap').hide();
+        } else {
+        }
+    });
 });
