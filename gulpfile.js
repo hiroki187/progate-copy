94765e1f65d3c26d67c40102129c5fd165d56532
ichionse_hiroki <ichionse_hiroki@Yuta-Ichinose.local>
2021-02-16 01:44:00 +0900

    完成

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require ('gulp');
-var sass = require ('gulp-sass');
-
-gulp.task('sass', function () {
-    gulp.src('sass/style.scss')
-    .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(gulp.dest(css/style.css))
-})
